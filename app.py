bbd8a7b7d38c7e6384e253319cc9210f12cff8ba
zainsherin <zainabsherin68@gmail.com>
2025-09-28 11:41:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from PIL import Image
+import cv2
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-import numpy as np
 
-# Load image
-image = Image.open("sample.jpg")
-image = image.resize((150, 150))  # resize to make processing faster
-image_np = np.array(image)
-image_np = image_np.reshape((-1, 3))  # flatten pixels
-
-# Extract main colors
+img = cv2.imread("poster.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+pixels = img.reshape(-1, 3)
 kmeans = KMeans(n_clusters=5)
-kmeans.fit(image_np)
-colors = kmeans.cluster_centers_.astype(int)
+kmeans.fit(pixels)
 
-# Show colors
-plt.figure(figsize=(8, 2))
-plt.imshow([colors])
-plt.axis("off")
+colors = kmeans.cluster_centers_.astype(int)
+plt.figure(figsize=(8,2))
+for i, color in enumerate(colors):
+    plt.subplot(1, 5, i+1)
+    plt.imshow([[color/255]])
+    plt.axis("off")
 plt.show()
+def rgb_to_hex(rgb):
+    return "#{:02x}{:02x}{:02x}".format(rgb[0], rgb[1], rgb[2])
+
+hex_colors = [rgb_to_hex(c) for c in colors]
+print(hex_colors)
